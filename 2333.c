ad685440e40917692b8e207349440d6d55657ce6
Poly000 <1348292515@qq.com>
2019-04-21 22:17:30 +0800

    make custom able

--- 2333.c
+++ 2333.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#define num_x 256
+#define num_y 1024
+#define num_z 1024
 
-char a[1024][1024][1024];
+int a[num_x][num_y][num_z];
 int main() {
 	int x=0,y=0,z=0;
 a:
-	a[x][y][z++]='2';
-	a[x][y][z++]='3';
-	a[x][y][z++]='3';
-	a[x][y][z++]='3';
-	if(z==1024) {
+	a[x][y][z++]=2;
+	a[x][y][z++]=3;
+	a[x][y][z++]=3;
+	a[x][y][z++]=3;
+	if(z==num_z) {
 		z=0;
-		if(++y==1024) {
-			if(++x==1024) {
+		if(++y==num_y) {
+			if(++x==num_x) {
 				scanf("%d",&x);
 				return 0;
 			} else {
