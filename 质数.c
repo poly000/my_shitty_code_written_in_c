0a6a32d52861e3697ccd514defa699adb07ef50a
poly000 <1348292515@qq.com>
2019-05-08 22:41:36 +0800

    优化

--- 质数.c
+++ 质数.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
+#define many 2000
+unsigned long num_[many]= {2};
 int main() {
-	register unsigned long x=3,i,n=2,amount=2;
-	printf("%10d%10d",2,3);
-a:
-	i=3;
-b:
-	if(x%i==0) {
-		x+=2;
-		goto a;
+#if many==1
+	printf("2\n");
+	return 0;
+#endif
+#if many>1
+	printf("%10d",2);
+	register unsigned long i=0,num=3,j=1;
+	do {
+		while(i<j) {
+			if(num%num_[i]==0)
+				break;
+			i++;
+		}
+		if(i==j) {
+			num_[i]=num;
+			j++;
+		}
+		i=0;
+		num+=2;
+	} while(j<many);
+	j=1;
+	while(++i<many) {
+		if(++j==10) {
+			printf("%10lu\n",num_[i]);
+			j=0;
+		} else
+			printf("%10lu",num_[i]);
 	}
-	i+=2;
-	if(i<x/2)
-		goto b;
-	if(++n==10) {
-		printf("%10lu\n",x);
-		n=0;
-	} else
-		printf("%10lu",x);
-	if(++amount==100)
-		return 0;
-	x+=2;
-	goto a;
+#endif
 }
