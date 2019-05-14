bfc3e0f47ce594a8b943ac206e0fb7f532b84438
poly000 <1348292515@qq.com>
2019-05-14 12:58:44 +0800

    使用指针

--- 2333.c
+++ 2333.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
-#define num_x 1024 
-#define num_y 1024
+#include <stdlib.h>
 #define num_z 1024
+#define num_y 1024
+#define num_x 1024
 
-char a[num_x][num_y][num_z];
 int main() {
+	register char *a=malloc(sizeof(char)*(num_x*num_y*num_z));
+	if(a==0)
+		return 1;
 	register int x=0,y=0,z=0;
 a:
-	a[x][y][z++]='2';
-	a[x][y][z++]='3';
-	a[x][y][z++]='3';
-	a[x][y][z++]='3';
+	*(a+x*num_y*num_z+y*num_z+z++)='2';
+	*(a+x*num_y*num_z+y*num_z+z++)='3';
+	*(a+x*num_y*num_z+y*num_z+z++)='3';
+	*(a+x*num_y*num_z+y*num_z+z++)='3';
 	if(z==num_z) {
 		z=0;
 		if(++y==num_y) {
+			y=0;
 			if(++x==num_x) {
 				printf("done.\n");
 				getchar();
 				return 0;
-			} else {
-				y=0;
-				goto a;
 			}
+			goto a;
 		}
 	}
 	goto a;
