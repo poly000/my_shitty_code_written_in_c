0f78a20e313c8cbb2aa0d1751478e5110308c8b8
poly000 <1348292515@qq.com>
2019-05-14 12:26:37 +0800

    修改输出

--- 零钱.c
+++ 零钱.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-	register int *n=malloc(sizeof(int)*5);
+	register int *n=malloc(sizeof(int)*5),yuan,jiao,fen;
 	if(n==0)
-		return 1; 
-	register int yuan,jiao,fen;
+		return 1;
 	double total;
 	printf("Please type num of\n1 fen, 2 fen, 5 fen, 1 jiao, 5 jiao: \n\n");
 	scanf("%d%d%d%d%d",n,n+1,n+2,n+3,n+4);
@@ -13,7 +12,7 @@ int main(){
 	yuan=(int)total;
 	fen=(int)(total*100)%10;
 	jiao=(total-yuan-fen*0.01)*10;
-	printf("\n%.2lf Total\n",total);
+	printf("\n\tTotal\r%.2lf\n",total);
 	printf("\n%d yuan %d jiao %d fen\n",yuan,jiao,fen);
 	return 0;
 }
