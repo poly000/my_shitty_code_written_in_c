662792913e7c96360a6c093d4da581d1386e93f4
poly000 <1348292515@qq.com>
2019-05-16 22:20:15 +0800

    ...

--- build(Git for win+Dev-cpp)/build-32.sh
+++
@@ -1,3 +0,0 @@
-for a in `ls ..`
-do "C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\gcc.exe" "$a" -o "${a%%.c}-x86.exe" -m32 -std=gnu99 -O3 -pipe -I "C:\Program Files (x86)\Dev-Cpp\MinGW64\include" -I "C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\include" -I "C:\Program Files (x86)\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include" -L "C:\Program Files (x86)\Dev-Cpp\MinGW64\lib" -L "C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\lib" -static-libgcc -m32
-done
