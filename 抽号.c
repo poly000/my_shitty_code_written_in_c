1239121c0324ca44596ccfd1e992d7a90b45d9bd
poly000 <1348292515@qq.com>
2019-05-19 23:10:31 +0800

    优化

--- 抽号.c
+++ 抽号.c
@@ -4,19 +4,20 @@
 
 int main() {
 	unsigned long N;
-	register unsigned long j,i,*k,o=0;
+	register unsigned long j,i,*k,l;
 	FILE *fp;
 	fprintf(stderr,"Please type how many numbers are there: ");
 	fscanf(stdin,"%lu",&N);
 	k=(unsigned long *)malloc(sizeof(unsigned long)*N);
 	if(k==0)
 		return 1;
-	for(i=0;i<N;i++)
+	for(i=0;i<N;i++) 
 		k[i]=0;
 	fp=fopen("output.txt","w+");
+	l=time(0);
 	for(j=0; j<N; j++) {
 		do {
-			srand(time(0));
+			srand(l++);
 			i=(unsigned long)rand()%N;
 		} while(k[i]!=0);
 		k[i]=1;
