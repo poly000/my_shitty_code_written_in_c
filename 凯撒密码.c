155a4cfc7adb8228600d44a84b2897b81ab600a2
poly000 <1348292515@qq.com>
2019-05-22 13:23:59 +0800

    凯撒密码

---
+++ 凯撒密码.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int iWeishu,i,j;
+	char a[1025],*p=a;
+	FILE *fp;
+	fp=fopen("output.txt","w+");
+	fclose(fp);
+a:
+	scanf("%s",a);
+	fp=fopen("output.txt","a+");
+	iWeishu=strlen(a);
+	for(j=-25; j<26; j++) {
+		fprintf(fp,"%3d: ",j);
+		for(i=0; i<iWeishu; i++)
+			fputc((char)((int)*(p+i)+j),fp);
+		fputc('\n',fp);
+	}
+	fclose(fp);
+	goto a;
+}
