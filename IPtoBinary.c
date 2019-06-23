abafba0006b75b9aeeee77766ef697a5d8bff1e0
Poly000 <1348292515@qq.com>
2019-06-23 19:12:10 +0800

    没用的commit

--- IPtoBinary.c
+++ IPtoBinary.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 int main() {
 	int iNum[4];
-	register int iDouble,i,j;
-	scanf("%d.%d.%d.%d",iNum,iNum+1,iNum+2,iNum+3);
+	register unsigned short iDouble,i,j;
+	scanf("%u.%u.%u.%u",iNum,iNum+1,iNum+2,iNum+3);
 	for(i=0; i<4; i++){
-		if(*(iNum+i)>255)
-			return 0;
+		if(*(iNum+i)>255){
+			printf("\n%u ERROR: BAD NUM\n",i+1);
+			return 1;
+		}
 		iDouble=256;
 		for(j=0; j<8; j++){
 			iDouble>>=1;
