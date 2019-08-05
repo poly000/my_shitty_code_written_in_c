382f65b0344f29bf9ed76e52b26a26e17ad0f8bb
poly000 <1348292515@qq.com>
2019-08-05 15:20:13 +0800

    优化

--- IPtoBinary.c
+++ IPtoBinary.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-	int iNum[4];
-	register unsigned short iDouble,i,j;
+	unsigned int iNum[4];
+	register unsigned char iDouble,i,j;
 	scanf("%u.%u.%u.%u",iNum,iNum+1,iNum+2,iNum+3);
 	for(i=0; i<4; i++){
-		if (*(iNum+i) > 255){
-			printf("\n%u ERROR: BAD NUM\n",i+1);
-			return 1;
-		}
-		iDouble = 256;
+		iDouble = 128;
 		for (j=0; j<8; j++){
-			iDouble >>= 1;
 			if ((*(iNum+i)&iDouble) == iDouble)
 				putchar('1');
 			else
 				putchar('0');
+			iDouble >>= 1;
 		}
 		putchar(' ');
 	}
-	putchar('\b\n');
+	putchar('\n');
 	return 0;
 }
