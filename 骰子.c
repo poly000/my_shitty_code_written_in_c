a455afde7570cd121c9e4a591d25db44de679304
poly000 <1348292515@qq.com>
2019-08-12 01:00:32 +0800

    修bug

--- 骰子.c
+++ 骰子.c
@@ -6,19 +6,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-time_t tTime = time(0);
-
-char cGetNu(void){
+char cGetNu(time_t tTime){
   srand(tTime++);
   return rand()%6+1;
 }
 
 int main(void) {
+  time_t tTime = time(0);
   register int iType;
   printf("Press any key to start: ");
   getchar();
   do {
-    printf("\nReslut: %d\nPress ENTER to continue, or any key else to quit.\n",cGetNu());
+    printf("\nReslut: %d\nPress ENTER to continue, or any key else to quit.\n",cGetNu(tTime++));
     iType = getchar();
   } while(iType=='\n');
   return 0;
